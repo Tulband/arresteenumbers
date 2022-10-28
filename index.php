126b1d9d76fd23dae9f10b455758d89567734a34
Maarten <maarten@rodekanarie.nl>
2022-10-28 21:43:19 +0200

    fix rendering of form at proper time

--- index.php
+++ index.php
@@ -11,11 +11,13 @@ if(isset($_POST['Submit']))
     // code for check server side validation
     if(empty($_SESSION['captcha_code'] ) || strcasecmp($_SESSION['captcha_code'], $_POST['captcha_code']) != 0)
     {
-        echo "not validated";
-
+        $validated = false;
+        $message = "The captcha was not correct. Please try again!";
     }
     else // Captcha verification is Correct. Final Code Execute here!		
     {
+        $validated = true;
+
         $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         $nickname = $_POST['name'];
         $nickname = str_replace(";",":",$nickname);
@@ -53,54 +55,63 @@ if(isset($_POST['Submit']))
         fflush($handle);
         fclose($handle);
 
-        echo("Jo " . $nickname . ", you will be number " . $new_number . ", remember this!");
+        $message=  "Jo " . $nickname . ", you will be number " . $new_number . ", remember this!";
     }
 }
 ?>
 
 
 <html>
-<head>
-<meta charset="utf-8">
-<title>Your arrestee number generator</title>
-<link href="./css/style.css" rel="stylesheet">
-<script type='text/javascript'>
-function refreshCaptcha(){
-	var img = document.images['captchaimg'];
-	img.src = img.src.substring(0,img.src.lastIndexOf("?"))+"?rand="+Math.random()*1000;
-}
-</script>
-</head>
-<body>
-<div id="frame0">
-    <h1>Your arrestee number generator</h1>
-    <p>Only take one number! </p>
-</div>
-<br>
 
-<form action="" method="post" name="form1" id="form1" >
-    <table>
-        <tr>
-            <td>Action name (not your real name!):</td>
-        </tr>
-        <tr>
-            <td><input type="text" name="name"></td>
-        </tr>
-        <tr>
-            <td>
-                Captcha: <img src="captcha.php?rand=<?php echo rand();?>" id='captchaimg'><br>
-                <i>Can't read the image? click <a href='javascript:
-refreshCaptcha();'>here</a> to refresh.</td></i>
-            </td>
-        <tr>
-            <td>
-                <input id="captcha_code" name="captcha_code" type="text">
-            </td>
-        </tr>
-        </tr>
-    <br>
-    <td><input name="Submit" type="submit" onclick="return validate();" value="Submit" class="button1"></td>
-    </table>
-</form>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title>Arrestee number generator</title>
+        <link href="./css/style.css" rel="stylesheet">
+        <script src="js/script.js"></script>
+    </head>
+
+    <body>
+        <div>
+            <h1>Arrestee number generator</h1>
+        </div>
+        <br>
+
+<?php
+if (isset($_POST['Submit']))
+{
+    echo $message;
+}
+if (!$validated)
+{
+?>
+        <form action="" method="post" name="form1" id="form1" >
+            <table>
+                <tr>
+                    <td>Action name (not your real name!):</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td>
+                        Captcha: <img src="captcha.php?rand=<?php echo rand();?>" id='captchaimg'><br>
+                        <i>Can't read the image? click <a href='javascript:
+        refreshCaptcha();'>here</a> to refresh.</td></i>
+                    </td>
+                <tr>
+                    <td>
+                        <input id="captcha_code" name="captcha_code" type="text">
+                    </td>
+                </tr>
+                </tr>
+            <br>
+            <td><input name="Submit" type="submit" onclick="return validate();" value="Submit" class="button1"></td>
+            </table>
+        </form>
+<?php
+}
+?>
+
+
+    </body>
 </html>
